21a73750e487956685e03291db7276aa4ac791e9
qxw <qu.xiawei@whaley.cn>
2018-04-28 14:41:15 +0800

    Remove useless modules

--- src/main/kotlin/com/heqing/Application.kt
+++ src/main/kotlin/com/heqing/Application.kt
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView
-import java.sql.DriverManager
 
 @MapperScan("com.heqing.dao")
 @SpringBootApplication
